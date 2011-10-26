b59767be487ca4311a1fee6f5ae9ef03b61fd23e
Chris Worfolk <xmeltrut@gmail.com>
2011-10-26 12:42:54 +0100

    Configuration file

---
+++ library/config.sample.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Configuration file
+ */
+
+define("DB_HOST", "localhost");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
+
+define("DB_PREFIX", "tbl_");
+define("PASSWORD_SALT", "");
+
+define("ROOT", "/index.php");
