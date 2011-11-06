1953eaf759ae9468643fc766d0e213e7af454e80
Chris Worfolk <xmeltrut@gmail.com>
2011-11-06 15:26:05 +0000

    Re-adding controllers with proper case

---
+++ application/admin/controllers/EventsController.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Events administration.
+ *
+ * @author Chris Worfolk <chris@societaspro.org>
+ * @package SocietasPro
+ * @subpackage Admin
+ */
+
+class EventsController extends BaseController implements iController {
+
+	private $instance;
+	
+	function __construct () {
+	
+		parent::__construct();
+		
+		// create a model
+		require_once("models/EventsModel.php");
+		$this->model = new EventsModel();
+	
+	}
+	
+	/**
+	 * Create a new event
+	 */
+	public function create () {
+	
+		// check for actions
+		if (reqSet("action") == "create") {
+			$this->model->write($_REQUEST);
+			$this->engine->assign("msg", $this->model->getMessage());
+		}
+		
+		// output the page
+		$this->engine->assign("form", $this->standardForm("create"));
+		$this->engine->display("events/create.tpl");
+	
+	}
+	
+	/**
+	 * Edit an event
+	 */
+	public function edit () {
+	
+		// check for actions
+		if (reqSet("action") == "edit") {
+			$this->model->write($_REQUEST, FrontController::getParam(0));
+			$this->engine->assign("msg", $this->model->getMessage());
+		}
+		
+		// get the object
+		$event = $this->model->getById(FrontController::getParam(0));
+		
+		// output page
+		$this->engine->assign("form", $this->standardForm("edit", $event->getAllData()));
+		$this->engine->display("events/edit.tpl");
+	
+	}
+	
+	/**
+	 * Default page
+	 */
+	public function index () {
+	
+		// check for actions
+		if (reqSet("action") == "mass") {
+			if ($info = $this->determineMassAction()) {
+				switch ($info["action"]) {
+					case "clone":
+						$this->model->cloneById($info["ids"]);
+						break;
+					case "delete":
+						$this->model->deleteById($info["ids"], 21);
+						break;
+				}
+			}
+			$this->engine->assign("msg", $this->model->getMessage());
+		}
+		
+		// gather page variables
+		$pageNum = FrontController::getParam(0);
+		$totalPages = totalPages($this->model->count());
+		$events = $this->model->get($pageNum);
+		
+		// output the page
+		$this->engine->assign("events", $events);
+		$this->engine->assign("totalPages", $totalPages);
+		$this->engine->display("events/index.tpl");
+	
+	}
+	
+	/**
+	 * Create a standard form for editing events
+	 *
+	 * @param string $action Form variable
+	 * @param array $data Default values
+	 */
+	private function standardForm ($action, $data = array()) {
+	
+		// create a form object
+		require_once("classes/FormBuilder.php");
+		$form = new FormBuilder();
+		
+		// create a location model
+		include_once("models/LocationsModel.php");
+		$locationsModel = new LocationsModel();
+		
+		// build an array of locations
+		$options = array();
+		$options[0] = LANG_UNKNOWN;
+		$locations = $locationsModel->get();
+		
+		foreach ($locations as $location) {
+			$options[$location->getData("locationID")] = $location->getData("locationName");
+		}
+		
+		// build the form
+		$form->addInput("name", LANG_NAME, arrSet($data, "eventName"));
+		$form->addSelect("location", LANG_LOCATION, $options, arrSet($data, "eventLocation"));
+		$form->addDateTime("date", LANG_DATE, arrSet($data, "eventDate"));
+		$form->addTextArea("description", LANG_DESCRIPTION, arrSet($data, "eventDescription"));
+		$form->addHidden("id", arrSet($data, "memberID"));
+		$form->addHidden("action", $action);
+		$form->addSubmit();
+		$form->setDefaultElement("name");
+		
+		return $form->build();
+	
+	}
+
+
+}
---
+++ application/admin/controllers/MailinglistController.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Mailing list controller
+ *
+ * @author Chris Worfolk <chris@societaspro.org>
+ * @package SocietasPro
+ * @subpackage Admin
+ */
+
+class MailinglistController extends BaseController implements iController {
+
+	private $instance;
+	
+	function __construct () {
+	
+		parent::__construct();
+		
+		// create a model
+		require_once("models/SubscribersModel.php");
+		$this->model = new SubScribersModel();
+	
+	}
+	
+	/**
+	 * Export as CSV
+	 */
+	public function csv () {
+	
+		// csv object
+		require_once("classes/CsvBuilder.php");
+		$csv = new CsvBuilder(LANG_MAILING_LIST);
+		
+		// get an array of members
+		$subscribers = $this->model->getAsArray();
+		
+		// begin output
+		$csv->addRow(array(LANG_EMAIL));
+		
+		// loop through subscribers
+		foreach ($subscribers as $subscriber) {
+			$data = array (
+				$subscriber
+			);
+			$csv->addRow($data);
+		}
+		
+		// log as an action in the audit trail
+		auditTrail(13);
+		
+		// output the result
+		$csv->output();
+	
+	}
+	
+	/**
+	 * Generate a list of subscribers
+	 */
+	public function generate () {
+	
+		// create a members model
+		require_once("models/MembersModel.php");
+		$membersModel = new MembersModel();
+		
+		// create a list
+		$subscribers = $this->model->getAsArray();
+		$members = $membersModel->getEmailsAsArray();
+		$all = array_merge($subscribers, $members);
+		$all = array_unique($all);
+		
+		$subscriberList = implode("; ", $all);
+		
+		// output page
+		$this->engine->assign("subscribers", $subscriberList);
+		$this->engine->display("mailinglist/generate.tpl");
+	
+	}
+	
+	/**
+	 * Mass import members
+	 */
+	public function import () {
+	
+		// check for actions
+		if (reqSet("action") == "import") {
+		
+			require_once("classes/ImportSubscribersWizard.php");
+			$wizard = new ImportSubscribersWizard();
+			
+			if ($_REQUEST["emails"] != "") {
+				$result = $wizard->import($_REQUEST["emails"]);
+			} elseif ($_FILES["upload"]["size"] > 0) {
+				$result = $wizard->import(file_get_contents($_FILES["upload"]["tmp_name"]));
+			}
+			
+			if (isset($result)) {
+				$this->engine->assign("msg", $result);
+			}
+		
+		}
+		
+		// build a form
+		require_once("classes/FormBuilder.php");
+		
+		$form = new FormBuilder();
+		$form->addTextArea("emails", LANG_SUBSCRIBERS);
+		$form->addFile("upload", LANG_FILE);
+		$form->addHidden("action", "import");
+		$form->addSubmit();
+		$form->setDefaultElement("emails");
+		
+		// output page
+		$this->engine->assign("form", $form->build());
+		$this->engine->display("mailinglist/import.tpl");
+	
+	}
+	
+	/**
+	 * Default page
+	 */
+	public function index () {
+	
+		// check for actions
+		if (reqSet("action") == "create") {
+			$this->model->create($_REQUEST["email"]);
+			$this->engine->assign("msg", $this->model->getMessage());
+		} elseif (reqSet("action") == "delete") {
+			$this->model->deleteById($_REQUEST["id"]);
+			$this->engine->assign("msg", $this->model->getMessage());
+		} elseif (reqSet("action") == "deleteByEmail") {
+			$this->model->deleteByEmail($_REQUEST["email"]);
+			$this->engine->assign("msg", $this->model->getMessage());
+		}
+		
+		// get list of recent subscribers
+		$recent = $this->model->get();
+		$this->engine->assign("recent", $recent);
+		
+		// output the page
+		$this->engine->display("mailinglist/index.tpl");
+	
+	}
+
+}
