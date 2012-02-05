6741d0773f6560c058caf1d2f407c25c618afcc3
Chris Worfolk <xmeltrut@gmail.com>
2012-02-05 15:21:09 +0000

    Fixing issues picked up by scanner

--- library/classes/TmpGateway.php
+++ library/classes/TmpGateway.php
@@ -28,10 +28,10 @@ class TmpGateway {
 		
 			// if we don't have the function, we'll have to create one
 			if (!function_exists('sys_get_temp_dir')) {
-				function sys_get_temp_dir() {
-					if ( $temp=getenv('TMP') )     return $temp;
-					if ( $temp=getenv('TEMP') )    return $temp;
-					if ( $temp=getenv('TMPDIR') )  return $temp;
+				function sys_get_temp_dir () {
+					if ( $temp=getenv('TMP') )		return $temp;
+					if ( $temp=getenv('TEMP') )		return $temp;
+					if ( $temp=getenv('TMPDIR') )	return $temp;
 					$temp = tempnam(__FILE__,'');
 					if (file_exists($temp)) {
 						unlink($temp);
